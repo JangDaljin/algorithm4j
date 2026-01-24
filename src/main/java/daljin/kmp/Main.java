f38630b3a75823c8b0baac10830b979323d2449a
JangDaljin <wonjin.jang@wantedlab.com>
2026-01-25 00:13:57 +0900

    feat: 백준 1786 성공

--- src/main/java/daljin/kmp/Main.java
+++ src/main/java/daljin/kmp/Main.java
@@ -11,12 +11,35 @@ public class Main {
     failure[0] = -1;
 
     for (int i = 1; i < pattern.length(); i++) {
-      int target = failure[i - 1] + 1;
-      char c = pattern.charAt(target);
+      char now = pattern.charAt(i);
+      char before = pattern.charAt(failure[i - 1] + 1);
 
-      if(c == pattern.charAt(i)) {
-        failure[i] =
+      if(before == now) {
+        failure[i] = failure[i - 1] + 1;
+      } else {
+        failure[i] = -1;
       }
+    }
+
+
+    int i = 0;
+    int j = 0;
+    while(i < text.length() && j < pattern.length()) {
+      if(text.charAt(i) == pattern.charAt(j)) {
+        j++;
+      } else {
+        if( j != 0 ) {
+          j = failure[j - 1] + 1;
+          continue;
+        }
+      }
+      i++;
+    }
+
+    if( j == pattern.length()) {
+      System.out.println("Pattern found at index: " + (i - pattern.length()));
+    } else  {
+      System.out.println("Pattern not found");
     }
   }
 
