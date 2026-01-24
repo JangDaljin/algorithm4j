a3c0ff575d79f84648a934472320e2b9bcbdc796
JangDaljin <toyyj15@naver.com>
2026-01-24 21:58:33 +0900

    feat: KMP 임시저장

--- src/main/java/daljin/kmp/Main.java
+++ src/main/java/daljin/kmp/Main.java
@@ -2,7 +2,23 @@ package daljin.kmp;
 
 public class Main {
 
-  static void main() {
-    System.out.println("Kmp");
+  static void main(String[] args) {
+
+    String text = "ABC ABCDAB ABCDABCDABDE";
+    String pattern = "ABCDABD";
+
+    int[] failure = new int[pattern.length()];
+    failure[0] = -1;
+
+    for (int i = 1; i < pattern.length(); i++) {
+      int target = failure[i - 1] + 1;
+      char c = pattern.charAt(target);
+
+      if(c == pattern.charAt(i)) {
+        failure[i] =
+      }
+    }
   }
+
+
 }
