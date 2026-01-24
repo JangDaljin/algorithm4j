52ac182e176f4e9c47fac9429b9469d6b886d4d1
Wonjin Jang <45301224+JangDaljin@users.noreply.github.com>
2026-01-25 00:17:51 +0900

    feat: 백준 1786

---
+++ src/main/java/daljin/kmp/Baekjoon1786.java
@@ -0,0 +1,76 @@
+package daljin.kmp;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Baekjoon1786 {
+
+  public static void main(String[] args) throws IOException {
+
+    BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+
+    String text = reader.readLine();
+    String pattern = reader.readLine();
+
+    int[] pi = new int[pattern.length()];
+    pi[0] = 0;
+    char now;
+    char before;
+    int j;
+    for (int i = 1; i < pattern.length(); i++) {
+      now = pattern.charAt(i);
+      before = pattern.charAt(pi[i - 1]);
+
+      if (now == before) {
+        pi[i] = pi[i - 1] + 1;
+      } else {
+        j = pi[i - 1];
+        while (true) {
+          if (j == 0) {
+            pi[i] = 0;
+            break;
+          }
+          j = pi[j - 1];
+          before = pattern.charAt(j);
+          if (now == before) {
+            pi[i] = j + 1;
+            break;
+          }
+        }
+      }
+    }
+
+    int count = 0;
+    List<Integer> indices = new ArrayList<>();
+
+    int n = 0;
+    int m = 0;
+    while (n < text.length()) {
+      if (text.charAt(n) == pattern.charAt(m)) {
+        m++;
+        n++;
+
+        if (m == pattern.length()) {
+          count++;
+          indices.add(n - m + 1);
+          m = pi[m - 1];
+        }
+        continue;
+      }
+
+      if (m == 0) {
+        n++;
+        continue;
+      }
+
+      m = pi[m - 1];
+    }
+
+    System.out.println(count);
+    System.out.print(
+        String.join(" ", indices.stream().map(String::valueOf).toArray(String[]::new)));
+  }
+}
--- src/main/java/daljin/kmp/Main.java
+++ src/main/java/daljin/kmp/Main.java
@@ -2,7 +2,46 @@ package daljin.kmp;
 
 public class Main {
 
-  static void main() {
-    System.out.println("Kmp");
+  static void main(String[] args) {
+
+    String text = "ABC ABCDAB ABCDABCDABDE";
+    String pattern = "ABCDABD";
+
+    int[] failure = new int[pattern.length()];
+    failure[0] = -1;
+
+    for (int i = 1; i < pattern.length(); i++) {
+      char now = pattern.charAt(i);
+      char before = pattern.charAt(failure[i - 1] + 1);
+
+      if(before == now) {
+        failure[i] = failure[i - 1] + 1;
+      } else {
+        failure[i] = -1;
+      }
+    }
+
+
+    int i = 0;
+    int j = 0;
+    while(i < text.length() && j < pattern.length()) {
+      if(text.charAt(i) == pattern.charAt(j)) {
+        j++;
+      } else {
+        if( j != 0 ) {
+          j = failure[j - 1] + 1;
+          continue;
+        }
+      }
+      i++;
+    }
+
+    if( j == pattern.length()) {
+      System.out.println("Pattern found at index: " + (i - pattern.length()));
+    } else  {
+      System.out.println("Pattern not found");
+    }
   }
+
+
 }
