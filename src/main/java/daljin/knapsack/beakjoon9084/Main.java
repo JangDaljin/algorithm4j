f413d5ae0e48ef3db8bee3d22c483666c0823c61
JangWonjin <jangwonjin@JangWonjinui-MacBookPro.local>
2026-01-29 18:20:43 +0900

    feat: baekjoon12865

---
+++ src/main/java/daljin/knapsack/beakjoon9084/Main.java
@@ -0,0 +1,55 @@
+package daljin.knapsack.beakjoon9084;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
+public class Main {
+
+  public static void main(String[] args) throws IOException {
+
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+    int T = Integer.parseInt(br.readLine());
+    int[] results = new int[T];
+
+    int N;
+    int[] coins;
+    int[] dp;
+
+    for (int i = 0; i < T; i++) {
+      //N
+      N = Integer.parseInt(br.readLine());
+
+      //coins
+      coins = new int[N];
+      String[] coinStrings = br.readLine().split(" ");
+      for (int j = 0; j < N; j++) {
+        coins[j] = Integer.parseInt(coinStrings[j]);
+      }
+
+      //M
+      int M = Integer.parseInt(br.readLine());
+
+      //dp
+      dp = new int[M + 1];
+      dp[0] = 1;
+      for (int coin : coins) {
+        for (int k = coin; k <= M; k++) {
+          dp[k] += dp[k - coin];
+        }
+      }
+
+      results[i] = dp[M];
+    }
+
+    //print
+    System.out.print(
+        String.join("\n",
+            IntStream.of(results)
+                .mapToObj(v -> String.valueOf(v))
+                .collect(Collectors.toList())));
+  }
+}
